a56969b7fe605dc0f5a6e0b007e93c6535de63c4
Sean Currlin <sean.currlin@gmail.com>
2024-01-14 12:34:40 -0500

    add index.js and navbar in components folder

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+const Navbar = () => {
+    return (
+        <div className="nav-container">
+            <div className="logo-container">
+                <Avatar src={icon} size="large" />
+                <Typography.Title level={2} className="logo">
+                    <Link to="/">Crypt0Watch</Link>
+                </Typography.Title>
+            </div>
+        </div>
+    )
+}
